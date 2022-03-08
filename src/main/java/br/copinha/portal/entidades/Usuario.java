041f40df00ebc43c6322ba4e92acac5bd9f9a943
bryan.melo <bryanrmelo@gmail.com>
2022-03-08 17:40:01 -0300

    Configuração de banco

---
+++ src/main/java/br/copinha/portal/entidades/Usuario.java
@@ -0,0 +1,18 @@
+package br.copinha.portal.entidades;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.persistence.Column;
+
+public class Usuario extends Entidade {
+    
+    @NotNull
+    @Size(min = 3, max = 80)
+    @Column(nullable = false, length = 80)
+    private String login;
+
+    @NotNull
+    @Size(min = 8)
+    @Column(nullable = false)
+    private String senha;
+}
