596f31bfd10f31b40f76908787427a30ae3d1c21
Bryan Melo <bryanrmelo@gmail.com>
2022-03-08 03:30:51 -0300

    J2EE pra Spring Boot

---
+++ src/test/java/br/copinha/portal/PortalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.copinha.portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
