4d5eda76f27e0d2f50e7301728173c99c64dabe7
bryan.melo <bryanrmelo@gmail.com>
2022-04-04 14:02:05 -0300

    Update

--- src/main/java/br/copinha/portal/entidades/Usuario.java
+++ src/main/java/br/copinha/portal/entidades/Usuario.java
@@ -3,7 +3,9 @@ package br.copinha.portal.entidades;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import javax.persistence.Column;
+import javax.persistence.Entity;
 
+@Entity
 public class Usuario extends Entidade {
     
     @NotNull
